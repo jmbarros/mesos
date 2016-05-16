d5ef21765df3b92d91851cfae4e83cee986bb492
JMBarros <jmbarros@jmbarros13.local>
2016-05-16 19:21:13 -0300

    install
    simple install

---
+++ install_mesos_ubuntu.sh
@@ -0,0 +1,7 @@
+#instal mesos
+#!/bin/bash
+apt-get update
+apt-get install -y tar wget git
+apt-get install -y openjdk-7-jdk
+apt-get install -y autoconf libtool
+apt-get -y install build-essential python-dev python-boto libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
